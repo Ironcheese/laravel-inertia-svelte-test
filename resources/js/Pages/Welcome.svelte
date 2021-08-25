067c1abcc678124f120ab5840346bd7aaa4d3b91
Alexander Friederich <alexander.friederich@gmail.com>
2021-08-25 10:16:56 +0200

    Works with persistent layout, but has dev warnings

--- resources/js/Pages/Welcome.svelte
+++ resources/js/Pages/Welcome.svelte
@@ -1,15 +1,12 @@
-<script>
-    import Layout from '../Shared/Layout.svelte';
-
-    export let user;
-</script>
-
-<svelte:head>
-    <title>Welcome</title>
-</svelte:head>
-
-<Layout>
-    <h1>Welcome</h1>
-    <p>Hello {user.name}, welcome to your first Inertia app!</p>
-    <button class="btn mt-5">Daisy-UI Button</button>
-</Layout>
+<script context="module">
+    import Layout from '../Shared/Layout.svelte'
+    export const layout = Layout
+  </script>
+  
+  <script>
+    export let user
+  </script>
+  
+  <h1>Welcome</h1>
+  <p>Hello {user.name}, welcome to your first Inertia app!</p>
+  <button class="btn mt-5">Daisy-UI Button</button>
