d0e6ccb3690ff7a9998531d290d50a6e6e8fe034
Alexander Friederich <alexander.friederich@gmail.com>
2021-08-25 10:28:32 +0200

    Now only the layout is rendered, the slot is completly ignored

--- resources/js/Pages/Welcome.svelte
+++ resources/js/Pages/Welcome.svelte
@@ -1,12 +1,12 @@
 <script context="module">
-    import Layout from '../Shared/Layout.svelte'
-    export const layout = Layout
-  </script>
-  
-  <script>
-    export let user
-  </script>
-  
-  <h1>Welcome</h1>
-  <p>Hello {user.name}, welcome to your first Inertia app!</p>
-  <button class="btn mt-5">Daisy-UI Button</button>
+import Layout from '../Shared/Layout.svelte'
+export const layout = Layout
+</script>
+
+<script>
+export let user
+</script>
+
+<h1>Welcome</h1>
+<p>Hello {user.name}, welcome to your first Inertia app!</p>
+<button class="btn mt-5">Daisy-UI Button</button>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,13 @@
-import { createInertiaApp } from '@inertiajs/inertia-svelte'
+import { createInertiaApp } from '@inertiajs/inertia-svelte';
+import Layout from './Shared/Layout.svelte';
 
 createInertiaApp({
-    resolve: name => require(`./Pages/${name}.svelte`),
+    // Using the example from here: https://inertiajs.com/pages#persistent-layouts
+    resolve: name => {
+        const page = require(`./Pages/${name}`).default;
+        page.layout = page.layout || Layout;
+        return page;
+    },
     setup({ el, App, props }) {
         new App({ target: el, props })
     },
